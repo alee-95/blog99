10e62dee4613c116e6298ec4c2dff2ea07b3f2a8
alee-95 <alejandryu95@gmail.com>
2024-11-21 15:11:20 -0300

    Agregado editar formulario

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,41 @@
 class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
-  end
-
-  def show
-    @article = Article.find(params[:id])
-  end
-
-  def new
-    @article = Article.new
-end
-
-# toma los params ya filtrados y crea el artículo: si se crea correctamente, lo muestro, si falla, muestro los errores en el mismo new
-def create
-    @article = Article.new(articles_params)
-    if @article.save
-      redirect_to @article
-    else
-      render :new, status: :unprocessable_entity
+    def index
+      @articles = Article.all
     end
-end
 
-private
-  def articles_params
-    params.require(:article).permit(:title, :body)
+    def show
+      @article = Article.find(params[:id])
+    end
+
+    def new
+      @article = Article.new
   end
+
+  # toma los params ya filtrados y crea el artículo: si se crea correctamente, lo muestro, si falla, muestro los errores en el mismo new
+  def create
+      @article = Article.new(articles_params)
+      if @article.save
+        redirect_to @article
+      else
+        render :new, status: :unprocessable_entity
+      end
+  end
+
+  def edit
+      @article = Article.find(params[:id])
+  end
+
+  def update
+      @article = Article.find(params[:id])
+      if @article.update(articles_params)
+        redirect_to @article
+      else
+        render :edit, status: :unprocessable_entity
+      end
+  end
+
+  private
+    def articles_params
+      params.require(:article).permit(:title, :body)
+    end
 end
